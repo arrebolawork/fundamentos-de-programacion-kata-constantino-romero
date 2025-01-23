45b5bf7814e52f47d73dcf1a3b994031f5c06a79
David Arrebola <arrebola.work@gmail.com>
2025-01-23 13:26:45 +0100

    ejercicio hecho

--- scripts.js
+++ scripts.js
@@ -1,41 +1,41 @@
 const pregunta = {
-    titulo: '¿A quién ha doblado más veces Constantino Romero?',
-    respuestas: [
-        {
-            label: "Clint Eastwood",
-            id: "clint-eastwood",
-            name: "actor",
-            value: "clint-eastwood"
-        },
-        {
-            label: "James Earl",
-            id: "james-earl",
-            name: "actor",
-            value: "james-earl"
-        },
-        {
-            label: "Roger Moore",
-            id: "roger-moore",
-            name: "actor",
-            value: "roger-moore"
-        },
-        {
-            label: "William Shatner",
-            id: "william-shatner",
-            name: "actor",
-            value: "william-shatner"
-        },
-        {
-            label: "Arnold Schwarzenegger",
-            id: "arnold-schwarzenegger",
-            name: "actor",
-            value: "arnold-schwarzenegger"
-        }
-    ]
-}
+  titulo: "¿A quién ha doblado más veces Constantino Romero?",
+  respuestas: [
+    {
+      label: "Clint Eastwood",
+      id: "clint-eastwood",
+      name: "actor",
+      value: "clint-eastwood",
+    },
+    {
+      label: "James Earl",
+      id: "james-earl",
+      name: "actor",
+      value: "james-earl",
+    },
+    {
+      label: "Roger Moore",
+      id: "roger-moore",
+      name: "actor",
+      value: "roger-moore",
+    },
+    {
+      label: "William Shatner",
+      id: "william-shatner",
+      name: "actor",
+      value: "william-shatner",
+    },
+    {
+      label: "Arnold Schwarzenegger",
+      id: "arnold-schwarzenegger",
+      name: "actor",
+      value: "arnold-schwarzenegger",
+    },
+  ],
+};
 
 function imprimePregunta(pregunta) {
-  const newHTML = "";
+  let newHTML = "";
 
   newHTML += imprimeTitulo(pregunta);
   newHTML += imprimeTodasLasRespuestas(pregunta);
@@ -45,22 +45,31 @@ function imprimePregunta(pregunta) {
 
 function imprimeTitulo(pregunta) {
   // Put your code here
+  return `<p>${pregunta.titulo}</p>`;
 }
 
 function imprimeTodasLasRespuestas(pregunta) {
   // Put your code here
+  let respuestas = "";
+  for (let i = 0; i < pregunta.respuestas.length; i++) {
+    respuestas += imprimeUnaRespuesta(i);
+  }
+  return respuestas;
 }
 
 function imprimeUnaRespuesta(respuesta) {
   // Put your code here
+  return imprimeLabel(respuesta) + imprimeInput(respuesta);
 }
 
 function imprimeLabel(respuesta) {
   // Put your code here
+  return `<label for=${pregunta.respuestas[respuesta].id}>${pregunta.respuestas[respuesta].label}</label>`;
 }
 
 function imprimeInput(respuesta) {
   // Put your code here
+  return `<input id=${pregunta.respuestas[respuesta].id} name=${pregunta.respuestas[respuesta].name} type="radio" value=${pregunta.respuestas[respuesta].value}>`;
 }
 
 document.getElementById("contenedorPreguntas").innerHTML = imprimePregunta(pregunta);
